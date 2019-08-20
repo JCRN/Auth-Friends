083343d3f5a9aca5980527b41da9e6027511a918
JCRN <clemons.jason@gmail.com>
2019-08-20 12:30:02 -0700

    added react-router deps, created Login component with setState

---
+++ friends/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Login() {
+    const [credentials, setCredentials] = useState([])
+
+    //handlechange
+    //handlesubmit
+
+    return(
+        
+    )
+}
